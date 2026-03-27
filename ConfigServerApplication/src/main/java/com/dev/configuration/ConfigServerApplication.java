e27265227a193c904791d0cef82acb8f376a6e4f
saumya-pande <saumyapande3@gmail.com>
2026-03-27 14:57:09 +0530

    config server made

--- ConfigServerApplication/src/main/java/com/dev/configuration/ConfigServerApplication.java
+++ ConfigServerApplication/src/main/java/com/dev/configuration/ConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.dev.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
